50f1aa32ad44393e890a01491d8c1621f7e51c2a
IsabelleRey <isabellrey@gmail.com>
2014-12-15 18:11:14 -0800

    first commit

---
+++ Tomatoes/MovieCell.h
@@ -0,0 +1,13 @@
+//
+//  MovieCell.h
+//  Tomatoes
+//
+//  Created by Zai on 12/13/14.
+//  Copyright (c) 2014 Isa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MovieCell : UITableViewCell
+
+@end
